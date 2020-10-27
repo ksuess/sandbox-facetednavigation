f5dbd2a9f1400f8103ebbf30236eb2c65a51dde5
Katja Suess <k.suess@rohberg.ch>
2020-10-27 11:23:09 +0100

    test Testcomponent

---
+++ src/customizations/theme/Footer/Footer.jsx
@@ -0,0 +1,82 @@
+/**
+ * Footer component.
+ * @module components/theme/Footer/Footer
+ */
+
+import React from 'react';
+import { Container, List, Segment } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import { FormattedMessage, defineMessages, injectIntl } from 'react-intl';
+import Testcomponent from 'addons/volto-facetednavigation/src/components/Testcomponent/Testcomponent';
+
+const messages = defineMessages({
+  copyright: {
+    id: 'Copyright',
+    defaultMessage: 'Copyright',
+  },
+});
+
+/**
+ * Component to display the footer.
+ * @function Footer
+ * @param {Object} intl Intl object
+ * @returns {string} Markup of the component
+ */
+const Footer = ({ intl }) => (
+  <Segment
+    role="contentinfo"
+    vertical
+    padded
+    inverted
+    color="grey"
+    textAlign="center"
+  >
+    <Container>
+      <Segment basic inverted color="grey" className="discreet">
+        <Testcomponent />
+      </Segment>
+      <List horizontal inverted>
+        {/* wrap in div for a11y reasons: listitem role cannot be on the <a> element directly */}
+        <div role="listitem" className="item">
+          <Link className="item" to="/sitemap">
+            <FormattedMessage id="Site Map" defaultMessage="Site Map" />
+          </Link>
+        </div>
+        <div role="listitem" className="item">
+          <Link className="item" to="/accesibility-info">
+            <FormattedMessage
+              id="Accessibility"
+              defaultMessage="Accessibility"
+            />
+          </Link>
+        </div>
+        <div role="listitem" className="item">
+          <Link className="item" to="/contact-form">
+            <FormattedMessage id="Contact" defaultMessage="Contact" />
+          </Link>
+        </div>
+        <div role="listitem" className="item">
+          <a className="item" href="https://plone.com">
+            <FormattedMessage
+              id="Powered by Plone & Python"
+              defaultMessage="Powered by Plone & Python"
+            />
+          </a>
+        </div>
+      </List>
+    </Container>
+  </Segment>
+);
+
+/**
+ * Property types.
+ * @property {Object} propTypes Property types.
+ * @static
+ */
+Footer.propTypes = {
+  /**
+   * i18n object
+   */
+};
+
+export default injectIntl(Footer);
